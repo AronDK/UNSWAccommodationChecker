5428d729e23161a25d3ca76f8c0201a3901de1de
Aron Keness <z5371654@ad.unsw.edu.au>
2023-08-20 02:36:34 +1000

    Fixed issue with response, returns object with fields regarding 2023 and 2024. Scraper is done, need to implement persistance, perminance and perhaps automated emails when open

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,22 @@ axios(config.url)
     .then((response: AxiosResponse) => {
         const html = response.data;
         const $ = load(html); 
-        const elements: any[] = [];
-        $("h2.application-updates", html).each(function() {
-            console.log("found element");
-            const body = $(this).text();
-            const text = $(this).find('p').text();
-            elements.push({
-                body,
-                text
-            });
+ 
+        const res: any = {
+            res24: Boolean, 
+            res23: Boolean 
+        };
+        res.res24 = false;
+        res.res23 = false;
+
+        $(".inner", html).each(function() {
+            const text24 = $(this).find("p").text().includes("Applications are now open for 2024!");
+            const text23 = $(this).find("p").text().includes("Applications are now open for 2023!");
+
+            res.res24 = text24 || res.res24;
+            res.res23 = text23 || res.res23;
         });
-        console.log(elements);
+        console.log(res);
     }).catch((err: Error) => console.log(err));
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
