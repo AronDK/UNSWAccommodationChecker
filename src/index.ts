cd1eccedb5483f73ef39e02a24f4018b4eab54cc
Aron Keness <z5371654@ad.unsw.edu.au>
2023-08-20 00:39:12 +1000

    Implemented axios response. Isolated element with applications notification

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import express, { Request, Response } from 'express';
 import axios, { AxiosResponse } from 'axios';
-import cheerio from 'cheerio';
-import jquery from 'jquery';
+import cheerio, { load } from 'cheerio';
 import config from './config.json'; 
 
 const app = express();
 app.use(express.json());
 
 const PORT: number = parseInt(process.env.PORT || config.port);
+
+axios(config.url)
+    .then((response: AxiosResponse) => {
+        const html = response.data;
+        const $ = load(html); 
+        const elements: any[] = [];
+        $("h2.application-updates", html).each(function() {
+            console.log("found element");
+            const body = $(this).text();
+            const text = $(this).find('p').text();
+            elements.push({
+                body,
+                text
+            });
+        });
+        console.log(elements);
+    }).catch((err: Error) => console.log(err));
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
